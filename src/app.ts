0b3ce8b3f7c5be902e8cad630b541a38d1818ac3
Moges741 <mogesse741@gmail.com>
2026-02-22 12:02:14 +0300

    prepared the routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from 'express'
+import userRoutes from './routes/userRoutes';
 const app = express()
-
 app.use(express.json());
-// app.use("/users", userRoutes);
-
+app.use("/users", userRoutes);
 app.listen(5000, () => {
     console.log(`Server is Running 5000`)
 })
