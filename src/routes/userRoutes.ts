eada2a4ca5227904ffb425fc3ff1ab6789c7a839
Moges741 <mogesse741@gmail.com>
2026-02-22 12:11:58 +0300

    tested done

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,10 +3,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
-router.post('/users', createUser);
-router.put('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
+router.get('', getUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
