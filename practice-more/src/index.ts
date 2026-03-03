922735f84d4149508b0ce5de0b5abcf09638f030
Moges741 <mogesse741@gmail.com>
2026-03-03 15:15:27 +0300

    made

--- practice-more/src/index.ts
+++ practice-more/src/index.ts
@@ -1,11 +1,26 @@
-let employe: {
-    readonly id: number,
-    name: string,
-    display: (info: string) => void
-} = {
-    id : 33,
-    name : "Moges",
-    display : (info: string) => {
-        console.log(info)
-    }
+import { TaskManager } from "./taskManager";
+const manager = new TaskManager();
+
+const command = process.argv[2];
+const value = process.argv[3];
+
+switch(command) {
+    case "add":
+        manager.addTasks(value!);
+        break;
+    case 'list':
+        manager.listTasks();
+        break;
+    case 'complete':
+        manager.completeTask(Number(value));
+        break;
+    default:
+        console.log(`
+                    Commands:
+                    add "Task name"
+                    list
+                    complete TASK_ID
+                    delete TASK_ID
+                    `);
+
 }
